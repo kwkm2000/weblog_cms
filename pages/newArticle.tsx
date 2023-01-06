f6609932bea36b5532e1a0a4774dd062a532d1a4
kwkm2000 <kwkm.0802@gmail.com>
2023-01-06 18:23:52 +0900

    新規記事ページ作成

---
+++ pages/newArticle.tsx
@@ -0,0 +1,13 @@
+import styles from "../styles/Home.module.css";
+
+export default function NewArticle() {
+  return (
+    <main>
+      <h1>New Article</h1>
+      <form action="">
+        <input type="text" />
+        <textarea name="`" id="" cols="30" rows="10"></textarea>
+      </form>
+    </main>
+  );
+}
